e0c26138859f3b5b376e90e1bae1e3315b02211d
Samsung <bitup.games@gmail.com>
2023-02-05 06:19:02 +0100

    fix: did authentication promise

--- scripts/index-composites.js
+++ scripts/index-composites.js
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv'
 import createCeramicClient from '../utils/createCeramicClient.js'
 import createDID from '../utils/createDID.js'
 dotenv.config()
-const did = createDID()
-const ceramic = createCeramicClient()
+const did = await createDID()
+const ceramic = createCeramicClient(did)
 // Index the models into ceramic node
 console.log("Indexing models...")
 const mergedComposite = await readEncodedComposite(ceramic, './composites/merged-composite.json')
